26dc111ab379dfeb2d7942054672974a3ab7ea43
Night-crawler-P <parshwashah055@gmail.com>
2025-02-10 09:26:50 +0530

    if-else

---
+++ 2_basics.cpp
@@ -0,0 +1,22 @@
+// Basic if-else statements
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin >> age;
+    
+    if (age >= 18)
+    {
+        cout << "You Are eligible" << endl;
+        
+    }else
+    {
+        cout << "You are not eligible " << endl;
+        
+    }
+    
+        
+    return 0;
+}
